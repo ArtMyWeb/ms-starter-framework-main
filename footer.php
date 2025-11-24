b6234e8a6296e151e8a19496e6c8b8438ab04d77
Serhii Bondarenko <1flatron1993@gmail.com>
2025-11-24 16:15:57 +0000

    Add map and hours information to footer; remove old blog page

--- footer.php
+++ footer.php
@@ -10,6 +10,11 @@ if ( !$footer_copyright ) {
 $info_footer_contact_form_title = get_field('info_footer_contact_form_title', 'option');
 $info_footer_contact_form_subtitle = get_field('info_footer_contact_form_subtitle', 'option');
 $info_footer_contact_form = get_field('info_footer_contact_form', 'option');
+// map
+$info_map_title = get_field('info_map_title', 'option');
+$info_map_address = get_field('info_map', 'option');
+// time 
+$info_hours = get_field('info_hours', 'option');
 ?>
 
 <footer id="site-footer" class="relative overflow-hidden bg-[url('/wp-content/uploads/2025/11/footer-bg.png')] no-repeat bg-cover">
@@ -35,11 +40,10 @@ $info_footer_contact_form = get_field('info_footer_contact_form', 'option');
                 </div>
                 <div class="place-info flex flex-col gap-5">
                     <div class="footer-map md:py-6 md:px-8 max-md:mb-10 [&>iframe]:h-full bg-dark p-[20px] ">
-                        <p class="text-white text-[1.5rem] font-archivo">Becker Legal | serving clients in New Hampshire & Maine</p>
-                        <p><iframe class="w-full" loading="lazy" style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3309.5961127483615!2d-84.54853862253039!3d33.95151467319519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f51423acf53315%3A0x68ca680dcf4f075b!2sJohnson%20%26%20Alday%2C%20LLC%20-%20Personal%20Injury%20%26%20Workers'%20Compensation!5e0!3m2!1sen!2sus!4v1759717983529!5m2!1sen!2sus" width="100%" height="400" allowfullscreen="allowfullscreen"></iframe></p>
-
-
-
+                        <p class="text-white text-[1.5rem] font-archivo"> <?php echo esc_html($info_map_title); ?></p>
+                        <p>
+                            <?php echo $info_map_address  ?>
+                        </p>
                     </div>
                     <div class="contacts grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] lg:grid-cols-[3fr_3fr_5fr] gap-1 px-5 py-[30px] bg-dark">
                         <div class="footer-widget-column">
@@ -53,7 +57,7 @@ $info_footer_contact_form = get_field('info_footer_contact_form', 'option');
                             <a href="tel:6032596726">603-259-6726</a>
                         </div>
                         <div class="footer-widget-column call-us flex flex-col gap-4">
-                            <h3>24/7 Live Call Answering</h3>
+                            <h3><?php echo esc_html($info_hours); ?></h3>
                             <div class="divider h-[1px] bg-[#FAF1A7] w-full"></div>
                             <a href="tel:6032596726">603-802-8100</a>
                         </div>
