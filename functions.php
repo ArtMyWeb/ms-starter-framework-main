b9cee70df065e9080199bef250c0e477494ebca1
Serhii Bondarenko <1flatron1993@gmail.com>
2025-11-26 14:36:32 +0000

    feat: Update footer with dynamic contact information and background image; remove unused fields

--- functions.php
+++ functions.php
@@ -393,4 +393,4 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 function custom_excerpt_more( $more ) {
 	return ' ...';
 }
-add_filter( 'excerpt_more', 'custom_excerpt_more' );
+add_filter( 'excerpt_more', 'custom_excerpt_more' ); 
