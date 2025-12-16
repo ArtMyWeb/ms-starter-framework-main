0ae9e464434de68d952dccfd5a3b6f49f67f4ad9
Serhii Bondarenko <1flatron1993@gmail.com>
2025-12-16 15:34:54 +0000

    Refactor blog section to use WP_Query for dynamic post retrieval and update markup for better SEO

--- _inc/flex/home-blog-grid.php
+++ _inc/flex/home-blog-grid.php
@@ -1,28 +1,54 @@
 <?php
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 3,
+    'post_status' => 'publish'
+);
+
+$blog_query = new WP_Query($args);
 ?>
 <section class="blog-section">
   <div class="container">
     <h2 class="text-center mb-10">Blog</h2>
     <div class="blog-grid grid grid-cols-1 col-3 md:grid-cols-2 lg:grid-cols-3 gap-10">
-      <?php if (have_rows('home_bg_items')): while (have_rows('home_bg_items')): the_row();
-        $img = get_sub_field('home_bg_item_image');
-        $ttl = get_sub_field('home_bg_item_title');
-        $exc = get_sub_field('home_bg_item_excerpt');
-        $lnk = get_sub_field('home_bg_item_link');
+      <?php
+      if ($blog_query->have_posts()) :
+          while ($blog_query->have_posts()) : $blog_query->the_post();
+              $post_id = get_the_ID();
+              $post_title = get_the_title();
+              $post_excerpt = get_the_excerpt();
+              $post_permalink = get_the_permalink();
+              $post_thumbnail = get_the_post_thumbnail_url($post_id, 'large');
       ?>
       <article class="blog-item group h-full overflow-hidden">
         <div class="relative">
           <div class="blog-img h-full max-h-[475px]">
-            <?php if (!empty($img['url'])): ?><img class="w-full h-auto object-cover aspect-[3/4]" src="<?php echo esc_url($img['url']); ?>" alt=""><?php endif; ?>
+            <?php if ($post_thumbnail): ?>
+              <img class="w-full h-auto object-cover aspect-[3/4]" src="<?php echo esc_url($post_thumbnail); ?>" alt="<?php echo esc_attr($post_title); ?>">
+            <?php else: ?>
+              <img class="w-full h-auto object-cover aspect-[3/4]" src="/wp-content/uploads/2025/11/Bg.png" alt="<?php echo esc_attr($post_title); ?>">
+            <?php endif; ?>
           </div>
           <div class="blog-content flex flex-col text-white z-10 vertical-border bg-light-dark/90 p-5 !absolute bottom-0 left-0 w-full">
-            <?php if ($ttl): ?><h3 class="text-[1.375rem]/[1.2] font-archivo font-semibold pb-2"><?php echo esc_html($ttl); ?></h3><?php endif; ?>
-            <div class="blog-excerpt"><div class=" "><div class=""><p><?php echo esc_html($exc ?: ''); ?></p></div></div></div>
-            <?php if (!empty($lnk['url'])): ?><a href="<?php echo esc_url($lnk['url']); ?>" class="text-light-gold text-[1rem]/[1.2] font-archivo font-medium underline underline-offset-5" target="<?php echo isset($lnk['target']) ? esc_attr($lnk['target']) : '_self'; ?>"><?php echo esc_html($lnk['title'] ?: 'Read More'); ?></a><?php endif; ?>
+            <h3 class="text-[1.375rem]/[1.2] font-archivo font-semibold pb-2">
+              <a href="<?php echo esc_url($post_permalink); ?>" class="hover:text-light-gold transition-colors">
+                <?php echo esc_html($post_title); ?>
+              </a>
+            </h3>
+            <div class="blog-excerpt"><div class=" "><div class=""><p><?php echo esc_html($post_excerpt); ?></p></div></div></div>
+            <a href="<?php echo esc_url($post_permalink); ?>" class="text-light-gold text-[1rem]/[1.2] font-archivo font-medium underline underline-offset-5 hover:text-white transition-colors">Read More</a>
           </div>
         </div>
       </article>
-      <?php endwhile; endif; ?>
+      <?php
+          endwhile;
+          wp_reset_postdata();
+      else :
+      ?>
+      <div class="col-span-full text-center py-10">
+        <p class="text-gray-600">No blog posts found.</p>
+      </div>
+      <?php endif; ?>
     </div>
   </div>
 </section>
